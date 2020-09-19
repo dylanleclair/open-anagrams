3c545b812041761a52dc79d9318d1bb1908a8bf8
dylanleclair <45674837+dylanleclair@users.noreply.github.com>
2020-09-19 16:47:19 -0600

    Initial commit

---
+++ Anagrams/Anagrams/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Anagrams
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var tree = new WordTree("words.txt");
+
+            while (true)
+            {
+                Console.Write("\n");
+                string input = Console.ReadLine();
+                Console.WriteLine(tree.FindWord(input));
+
+            }
+
+           
+
+        }
+
+
+
+    }
+}
